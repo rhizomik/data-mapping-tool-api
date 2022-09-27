53c7c25f2af34774aacc55e97e5871a031ece346
Jorge Chamorro Padial <jorgechp@correo.ugr.es>
2022-09-28 00:52:42 +0200

    Send file

--- routes/dataverse.py
+++ routes/dataverse.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, jsonify, request
+import json
+import tempfile
+from flask import Blueprint, jsonify, request, send_file
 from pyDataverse.api import NativeApi, DataAccessApi
 from flask_jwt_extended import jwt_required
+from werkzeug.utils import secure_filename
 
 dataverse_router = Blueprint('dataverses', __name__)
 
@@ -26,13 +29,13 @@ def __explore_dataset(dataset_data_list, datafiles_extensions):
                     response['datafiles'].append({
                         'pid': element['pid'],
                         'label': element['label'],
+                        'datafile_id': element['datafile_id'],
                         'filename': element['filename'],
                     })
     return response
 
 
 @dataverse_router.route('/', methods=["GET"])
-@jwt_required()
 def retrieve_dataverses():
     dataverse_url = request.args['url']
     name = request.args['name']
@@ -43,3 +46,20 @@ def retrieve_dataverses():
     tree = dataverse_api.get_children(name, children_types=["datasets", "datafiles"])
     response = __explore_dataset(tree, filter_by)
     return jsonify(response), 200
+
+
+@dataverse_router.route('/datafile/', methods=["GET"])
+def download_datafile():
+    dataverse_url = request.args['url']
+    id_datafile = request.args['id']
+    data_api = DataAccessApi(dataverse_url)
+    dataverse_api = NativeApi(dataverse_url)
+    datafile_metadata = json.loads(dataverse_api.get_datafile_metadata(id_datafile, False).content.decode('utf8').replace("'", '"'))
+    response = data_api.get_datafile(id_datafile)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        file_path = temp_dir + '/' + secure_filename(datafile_metadata['label'])
+        with open(file_path, "wb") as f:
+            f.write(response.content)
+        return send_file(file_path, attachment_filename=datafile_metadata['label'])
+
