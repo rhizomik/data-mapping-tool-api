994fdf90b491396315e9f7afc3f4169fa2ff5edb
Jorge Chamorro Padial <jorgechp@correo.ugr.es>
2022-09-29 23:49:52 +0200

    Improve dataverse exploration

--- routes/dataverse.py
+++ routes/dataverse.py
@@ -2,37 +2,10 @@ import json
 import tempfile
 from flask import Blueprint, jsonify, request, send_file
 from pyDataverse.api import NativeApi, DataAccessApi
-from flask_jwt_extended import jwt_required
+from pyDataverse.utils import dataverse_tree_walker
 from werkzeug.utils import secure_filename
 
 dataverse_router = Blueprint('dataverses', __name__)
-
-
-def __explore_dataset(dataset_data_list, datafiles_extensions):
-    response = {
-        'datafiles': [],
-        'datasets': []
-    }
-
-    for element in dataset_data_list:
-        if element['type'] == 'dataset':
-            dataset_data = {
-                'pid': element['pid'],
-                'children': __explore_dataset(element['children'], datafiles_extensions)
-            }
-            response['datasets'].append(dataset_data)
-        if element['type'] == 'datafile':
-            name = element['label']
-            if '.' in name:
-                extension = element['label'].split('.')[1]
-                if extension in datafiles_extensions or len(datafiles_extensions) == 0:
-                    response['datafiles'].append({
-                        'pid': element['pid'],
-                        'label': element['label'],
-                        'datafile_id': element['datafile_id'],
-                        'filename': element['filename'],
-                    })
-    return response
 
 
 @dataverse_router.route('/', methods=["GET"])
@@ -40,11 +13,43 @@ def retrieve_dataverses():
     dataverse_url = request.args['url']
     name = request.args['name']
     filter_by = request.args['filter_by'] if 'filter_by' in request.args else []
-    filter_by.split(',')
+    if len(filter_by) > 0:
+        filter_by.split(',')
     dataverse_api = NativeApi(dataverse_url)
+    tree = dataverse_api.get_children(name, children_types=["datasets", "datafiles", "dataverses"])
+    dataverses, datasets, datafiles = dataverse_tree_walker(tree)
+    response = {
+        'dataverses': dataverses,
+        'datafiles': datafiles,
+        'datasets': datasets
+    }
 
-    tree = dataverse_api.get_children(name, children_types=["datasets", "datafiles"])
-    response = __explore_dataset(tree, filter_by)
+    return jsonify(response), 200
+
+
+@dataverse_router.route('/dataset/', methods=["GET"])
+def explore_dataset():
+    dataverse_url = request.args['url']
+    id_dataset = request.args['id']
+    dataverse_api = NativeApi(dataverse_url)
+    metadata = dataverse_api.get_datafiles_metadata(id_dataset).content
+    metadata_dict = json.loads(metadata.decode("utf8"))['data']
+
+    response = {
+        'files': []
+    }
+    for file in metadata_dict:
+        response['files'].append(
+            {
+                'id': file['dataFile']['id'],
+                'persistentId': file['dataFile']['persistentId'],
+                'response': file['dataFile']['response'],
+                'fileName': file['dataFile']['filename'],
+                'contentType': file['dataFile']['contentType'],
+                'creationDate': file['dataFile']['creationDate'],
+            }
+        )
+
     return jsonify(response), 200
 
 
@@ -54,7 +59,10 @@ def download_datafile():
     id_datafile = request.args['id']
     data_api = DataAccessApi(dataverse_url)
     dataverse_api = NativeApi(dataverse_url)
-    datafile_metadata = json.loads(dataverse_api.get_datafile_metadata(id_datafile, False).content.decode('utf8').replace("'", '"'))
+    datafile_metadata = json.loads(dataverse_api
+                                   .get_datafile_metadata(id_datafile, False)
+                                   .content
+                                   .decode('utf8').replace("'", '"'))
     response = data_api.get_datafile(id_datafile)
 
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -62,4 +70,3 @@ def download_datafile():
         with open(file_path, "wb") as f:
             f.write(response.content)
         return send_file(file_path, attachment_filename=datafile_metadata['label'])
-
